d459ac12ed7e0c2c17c2b85a7c0e4d8f495e5cfe
kenlinkkkk <Kenlink12>
2019-05-17 23:54:11 +0700

    23:53 16-05-2019

---
+++ application/views/front_Lienhe.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thanh
+ * Date: 05/17/2019
+ * Time: 11:51 AM
+ */
+?>
+
+<main>
+    <div id="lienhe">
+        <div class="container vh-center">
+            <div class="row">
+                <div class="col-10 vh-center white-text">
+                    <p>Mọi thắc mắc xin liên hệ</p>
+                    <h5>CÔNG TY CỔ PHẦN VANO</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
